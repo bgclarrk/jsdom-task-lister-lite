3b47627b3e2b305b925c60071d39bec02ef291b4
+ <+>
2021-08-05 18:01:42 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  submitBtn = document.querySelector("#create-task-form > input[type=submit]:nth-child(3)")
+
+  submitBtn.addEventListener("click", function(e) {
+    console.log("Blocking submit");
+    e.preventDefault();
+    task = document.querySelector("#new-task-description").value;
+    document.querySelector("#tasks").innerHTML = task;
+  })
+  
 });
